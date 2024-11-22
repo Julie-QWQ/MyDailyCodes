eedc7501ddd88f4d16c5c6474cb3010628c6ba74
Julie <161043091+Julie-QWQ@users.noreply.github.com>
2024-11-22 23:31:24 +0800

    Update Test.c

--- DS_review/Test.c
+++ DS_review/Test.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
-#include "Array/Array.h"
-#include "List/List.h"
+#include "DataStruct.h"
 
 int main(void) {
-    List lst = GetList();
-    // ArrayResize(arr, maxSize);
-    for (int i=0; i<100; ++i) {
-        ListPushElem(lst, i*i);
+
+    Stack stk = GetStack();
+    for (int i=1; i<=100; ++i) {
+        StackPushElem(stk, i*i);
     }
-    PrintList(lst);
-    DeleteList(lst);
+    int cnt = 1;
+    while (!IsEmptyStack(stk)) {
+        printf("%d: %d\n", cnt++, StackPopElem(stk));
+    }
+    putchar('\n');
+    DeleteStack(stk);
 
-
+    puts("Done !");
     return 0;
 }
+
+// MakeFile:
+// gcc Test.c Array/Array.c List/Lists.c Stack/Stack.c Queue/Queue.c -o test.exe
