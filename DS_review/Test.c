cd1454338865d25314cec5b295f54fa514c5d448
Julie <161043091+Julie-QWQ@users.noreply.github.com>
2024-11-23 01:04:33 +0800

    Update Test.c

--- DS_review/Test.c
+++ DS_review/Test.c
@@ -5,6 +5,7 @@
 int main(void) {
 
     Stack stk = GetStack();
+    Queue qe = GetQueue();
     
     char string[50];
     scanf("%s", string);
@@ -12,12 +13,12 @@ int main(void) {
 
     while (*ptr) {
         if (isdigit(*ptr)) {
-            putchar(*ptr);
+            QueuePushElem(qe, *ptr);
         }
 
         else if (*ptr == '+' || *ptr == '-') {
             while (!IsEmptyStack(stk) && StackTopElem(stk) != '(') {
-                putchar(StackPopElem(stk));
+                QueuePushElem(qe, StackPopElem(stk));
             }
             StackPushElem(stk, *ptr);
         }
@@ -25,7 +26,7 @@ int main(void) {
         else if (*ptr == '*' || *ptr == '/') {
             char top;
             while (!IsEmptyStack(stk) && ((top = StackTopElem(stk)) == '*' || top == '/')) {
-                putchar(StackPopElem(stk));
+                QueuePushElem(qe, StackPopElem(stk));
             }
             StackPushElem(stk, *ptr);
         }
@@ -36,24 +37,56 @@ int main(void) {
 
         else if (*ptr == ')') {
             while (!IsEmptyStack(stk) && (StackTopElem(stk) != '(')) {
-                putchar(StackPopElem(stk));
+                QueuePushElem(qe, StackPopElem(stk));
             }
             StackPopElem(stk);
         }
 
         else {
-            putchar(*ptr);
+            QueuePushElem(qe, *ptr);
         }
 
         ++ptr;
     }
 
     while (!IsEmptyStack(stk)) {
-        putchar(StackPopElem(stk));
+        QueuePushElem(qe, StackPopElem(stk));
     }
 
-    putchar('\n');
+    while (!IsEmptyQueue(qe)) {
+        char top = QueuePopElem(qe);
 
+        if (isdigit(top)) {
+            StackPushElem(stk, top);
+        }
+
+        else {
+            int oprd1 = StackPopElem(stk) - '0';
+            int oprd2 = StackPopElem(stk) - '0';
+            int res;
+
+            switch (top) {
+                case '+':
+                    res = oprd2 + oprd1;
+                    break;
+                case '-':
+                    res = oprd2 - oprd1;
+                    break;
+                case '*':
+                    res = oprd2 * oprd1;
+                    break;
+                case '/':
+                    res = oprd2 / oprd1;
+                    break;
+            }
+
+            StackPushElem(stk, res + '0');
+        }
+    }
+
+    printf(" = %d\n", StackPopElem(stk) - '0');
+
+    DeleteQueue(qe);
     DeleteStack(stk);
 
     puts("Done !");
