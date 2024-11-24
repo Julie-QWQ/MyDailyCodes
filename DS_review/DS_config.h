d9d66951362dfcdc827203f0661fac56a985a1ef
Julie <161043091+Julie-QWQ@users.noreply.github.com>
2024-11-25 00:50:00 +0800

    Update DS_config.h

--- DS_review/DS_config.h
+++ DS_review/DS_config.h
@@ -1,6 +1,12 @@
 #ifndef __DS_CONFIG_H_
 #define __DS_CONFIG_H_
 
-typedef int ElemType;
+typedef int ArrayElemType;
+
+typedef int ListElemType;
+
+typedef int QueueElemType;
+
+typedef int StackElemType;
 
 #endif
